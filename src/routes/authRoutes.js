788b7656f61ecc068f4daef22d1507bb781f5c00
Gowtham <DEV@T2Sadmins-MacBook-Air.local>
2020-09-19 20:02:39 +0530

    removed unwanted files

--- src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-let express = require("express");
-const router = express.Router();
-let { getUserlogin, addUsers } = require("../controllers/UserController");
-
-router.get("/login", getUserlogin);
-router.post("/signup", addUsers);
-
-module.exports = router;
